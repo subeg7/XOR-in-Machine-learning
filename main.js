75255525ea271f448b11efcf3949ebd8f740d252
Subeg7 <subeg7@gmail.com>
2018-10-16 10:52:59 +0545

    first forward feed completed

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
   // document.write("inside setup");
   //creating network
   let network = new NeuralNetwork(2,2,1);
+
+  var input_arr= [0,1];
+  network.feedforward(input_arr);
+
+
 console.log("faster");
-  // network.feedforward();
-  // document.write(network.output_nodes)
--- network.js
+++ network.js
@@ -1,3 +1,11 @@
+//making sigmoid funciton(object of class AF) global
+  let sigmoid = new ActivationFunction(
+  x => 1 / (1 + Math.exp(-x)),
+  y => y * (1 - y)
+);
+
+
+//main NeuralNetwork class
 class NeuralNetwork{
   constructor(input_nodes, hidden_nodes,output_nodes){
     //init the nodes of each layer
@@ -11,6 +19,10 @@ class NeuralNetwork{
 
     //randomize within -1 to 1
     this.hi_weights.randomize();
+
+    console.log("hi_weights at init");
+    this.hi_weights.display();
+    // console.log("break");
     this.ho_weights.randomize();
 
     //init biases and randomize
@@ -22,22 +34,47 @@ class NeuralNetwork{
     //set learning_rate;
     this.learning_rate=0.1;
 
+    //initialize the activation function(sigmoid)
+    this.setActivationFunction();
 
-    this.bias =1 ;
   }
 
-  let sigmoid = new ActivationFunction(
-    x => 1 / (1 + Math.exp(-x)),
-    y => y * (1 - y)
-  );
-
+  //the default activation function is sigmoid
   setActivationFunction(func = sigmoid){
     this.activation_function = func;
   }
 
    feedforward(input_values){
+     console.log("input="+input_values);
 
-    //return output
+     let input_mat = Matrix.fromArray(input_values);
+
+     console.log("mat.data="+input_mat.data);
+
+      input_mat.display();
+     
+     let hidden = Matrix.multiply(this.hi_weights, input_mat);
+     console.log(hidden);
+
+     // let hidden= this.hi_weights.multiply(input_set);
+     hidden.add(this.bias_hidden);
+     //activation function
+     hidden.map(this.activation_function.func)
+
+     //for output layer
+     let output = Matrix.multiply(this.ho_weights, hidden);
+     output.add(this.bias_o);
+     output.map(this.activation_function.func);
+
+
+     console.log("output=");
+     console.log(output.data[0]);
+
+     //return final output
+  }
+
+  backpropagate(input_values,true_values){
+    //train the weights
   }
 
 
