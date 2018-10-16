0081170b55ebe308feb274e45a26f7ebce5ca6da
Subeg7 <subeg7@gmail.com>
2018-10-16 23:23:18 +0545

    parameters for backpropagation succeeded

--- main.js
+++ main.js
@@ -2,5 +2,8 @@
   //creating network
   let network = new NeuralNetwork(2,2,1);
 
-  var input_arr= [ [1,1] , [0,0], [0,1], [1,0] ];
-  network.feedforward(input_arr[ parseInt((Math.random()*4) )]);
+  var input_arr= [ [1,0] , [0,1], [1,1], [0,0] ];
+   let true_output=[[1],[1],[0],[0] ];
+  let ind=parseInt((Math.random()*4) );
+  let sets = network.feedforward(input_arr[ ind]);
+  network.backpropagate(true_output[ind],sets[0],sets[1],sets[2]);
