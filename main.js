04ab9c39e5f1aee0034a2a2f83de08c48944885d
Subeg7 <subeg7@gmail.com>
2018-10-16 11:19:03 +0545

    input set made dynamic

--- main.js
+++ main.js
@@ -2,8 +2,5 @@
   //creating network
   let network = new NeuralNetwork(2,2,1);
 
-  var input_arr= [0,1];
-  network.feedforward(input_arr);
-
-
-console.log("faster");
+  var input_arr= [ [1,1] , [0,0], [0,1], [1,0] ];
+  network.feedforward(input_arr[ parseInt((Math.random()*4) )]);
